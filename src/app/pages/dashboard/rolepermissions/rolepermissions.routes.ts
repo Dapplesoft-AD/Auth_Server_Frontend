75c6732b468f3e34d06f603cd4a5b9ad2a02e822
Hasan Uddin <abcnizamd@gmail.com>
2025-12-01 19:26:44 +0600

    🚧 auth_guarded pages

--- src/app/pages/dashboard/rolepermissions/rolepermissions.routes.ts
+++ src/app/pages/dashboard/rolepermissions/rolepermissions.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router'
 import { PageLayout, setLayout } from '../../../../libs/common-components'
+import { authGuard } from '../../../../libs/guards/auth/auth.guard'
 
 export type RolePermissionsRoutes = {
     rolepermissions: Route
@@ -8,6 +9,7 @@ export type RolePermissionsRoutes = {
 export const rolePermissionsRoutes: RolePermissionsRoutes = {
     rolepermissions: {
         path: 'admin/rolepermissions',
+        canActivate: [authGuard],
         loadComponent: () =>
             import(
                 './page-rolepermissions/page-rolepermissions.component'
