52e831c4dda8b02aaef5f475d6a71182bcba6080
Hasan Uddin <abcnizamd@gmail.com>
2025-12-22 23:29:26 +0600

    ➕cached auth state ✅

--- src/libs/auth/service/auth.service.ts
+++ src/libs/auth/service/auth.service.ts
@@ -1,32 +1,59 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable, inject } from '@angular/core'
 import { Router } from '@angular/router'
-import { catchError, map, Observable, of } from 'rxjs'
+import { BehaviorSubject, catchError, map, Observable, of, tap } from 'rxjs'
+import { authRoutes } from '../../../app/pages/auth/auth.route'
 import { environment } from '../../../environments/environment'
-import { ContextUserStorageService } from './contextUser-storage.service'
-import { TokenStorageService } from './token-storage.service'
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService {
     private router = inject(Router)
-    private tokenStorage = inject(TokenStorageService)
-    private contextUserIdStorageService = inject(ContextUserStorageService)
     private http = inject(HttpClient)
     private authApiUrl = `${environment.authApiUrl}`
 
+    // cached auth state
+    private authState$ = new BehaviorSubject<boolean | null>(null)
+    private redirecting = false
+
     isLoggedIn(): Observable<boolean> {
+        if (this.authState$.value !== null) {
+            return of(this.authState$.value)
+        }
         return this.http
             .get(`${this.authApiUrl}/me`, { withCredentials: true })
             .pipe(
                 map(() => true),
-                catchError(() => of(false)),
+                tap((v) => this.authState$.next(v)),
+                catchError(() => {
+                    this.authState$.next(false)
+                    return of(false)
+                }),
             )
     }
     logout() {
-        return this.http
+        this.http
             .post(`${this.authApiUrl}/logout`, {}, { withCredentials: true })
-            .subscribe(() => this.router.navigate(['/login']))
+            .subscribe(() => {
+                this.authState$.next(false)
+                this.router.navigate(['/login'])
+            })
+    }
+
+    clearAuthState() {
+        this.authState$.next(false)
+    }
+
+    setAuthenticated() {
+        this.redirecting = false
+        this.authState$.next(true)
+    }
+
+    handleUnauthorized() {
+        if (this.redirecting) return
+        this.redirecting = true
+        this.authState$.next(false)
+        this.router.navigate([authRoutes.login.path])
     }
 }
